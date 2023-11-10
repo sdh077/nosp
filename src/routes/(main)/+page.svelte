39c40e75f1819ac2abb49f68a2b4f9bf8193ea47
shindaeho <sdh077@urbancorp.co.kr>
2023-11-10 16:20:46 +0900

    링크 추가

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,45 +1,25 @@
 <script>
 	import Fullcalendar from '$lib/components/layout/Fullcalendar.svelte';
 	export let data;
+	console.log(data);
 	$: innerWidth = 0;
 </script>
 
 <svelte:window bind:innerWidth />
 {#if innerWidth < 1032}
 	<div class="p-4 space-y-4">
-		<div class="border rounded-lg p-4 space-y-2">
-			<h2 class="text-2xl font-semibold">Game 1</h2>
-			<p class="text-gray-600 dark:text-gray-300">This is a brief description for game 1.</p>
-			<button
-				class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
-				>Toggle Spoiler</button
-			>
-			<div class="border-t pt-2 text-gray-500 dark:text-gray-400">
-				<p>This is the spoiler content for game 1.</p>
+		{#each data.matches.slice(0, 5) as match}
+			<div class="border rounded-lg p-4 space-y-2">
+				<h2 class="text-2xl font-semibold">{match.title}</h2>
+				<p class="text-gray-600 dark:text-gray-300">{match.playDate.slice(0, 10)}</p>
+				<a href="/match/{match.matchId}">
+					<button
+						class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
+						>경기 보러가기</button
+					>
+				</a>
 			</div>
-		</div>
-		<div class="border rounded-lg p-4 space-y-2">
-			<h2 class="text-2xl font-semibold">Game 2</h2>
-			<p class="text-gray-600 dark:text-gray-300">This is a brief description for game 2.</p>
-			<button
-				class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
-				>Toggle Spoiler</button
-			>
-			<div class="border-t pt-2 text-gray-500 dark:text-gray-400">
-				<p>This is the spoiler content for game 2.</p>
-			</div>
-		</div>
-		<div class="border rounded-lg p-4 space-y-2">
-			<h2 class="text-2xl font-semibold">Game 3</h2>
-			<p class="text-gray-600 dark:text-gray-300">This is a brief description for game 3.</p>
-			<button
-				class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
-				>Toggle Spoiler</button
-			>
-			<div class="border-t pt-2 text-gray-500 dark:text-gray-400">
-				<p>This is the spoiler content for game 3.</p>
-			</div>
-		</div>
+		{/each}
 	</div>
 {:else}
 	<Fullcalendar matches={data.matches} />
